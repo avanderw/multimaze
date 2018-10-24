d5bd04b3e4ddd1f0c11b61838a2a483230826dbd
Andrew van der Westhuizen <avanderw@gmail.com>
2018-10-24 15:11:00 +0200

    updated to use sprite instead of image to make positioning easier

--- multimaze-desktop-client/src/main/java/net/avdw/multimaze/client/desktop/player/Player.java
+++ multimaze-desktop-client/src/main/java/net/avdw/multimaze/client/desktop/player/Player.java
@@ -2,22 +2,23 @@ package net.avdw.multimaze.client.desktop.player;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import org.pmw.tinylog.Logger;
 
 public class Player extends Actor {
-    private final Image image;
+    private final Sprite sprite;
 
     @Inject
     Player(@Named("player-texture") Texture texture) {
         Logger.trace(String.format("texture=%s", texture));
-        image = new Image(texture);
+        sprite = new Sprite(texture);
         setSize(texture.getWidth(), texture.getHeight());
+        setPosition(15, 15);
         addListener(new InputListener() {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -29,6 +30,12 @@ public class Player extends Actor {
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        image.draw(batch, parentAlpha);
+        sprite.draw(batch, parentAlpha);
+    }
+
+    @Override
+    public void setPosition(float x, float y) {
+        super.setPosition(x, y);
+        sprite.setPosition(x, y);
     }
 }
